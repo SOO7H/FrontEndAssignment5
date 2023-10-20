8a436142a318de9de2cd96d9008c654697461e77
MILIND JHA <milind.jha.20@gmail.com>
2023-10-20 10:13:26 +0530

    sort changes

--- js/shop.js
+++ js/shop.js
@@ -92,16 +92,16 @@ sort.innerHTML=`
                                         </button>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="triggerId" id="sort">
                                     <button class="btn btn-outline-black decrease"
-onclick="sortPriceIncreasing()"
+onclick="sort('pasc')"
 type="button">Price (Low to High)</button>
 <button class="btn btn-outline-black decrease"
-onclick="sortPriceDecreasing()"
+onclick="sort('pdesc')"
 type="button">Price (High to Low)</button>
 <button class="btn btn-outline-black decrease"
-onclick="sortRatingIncreasing()"
+onclick="sort('rasc')"
 type="button">Rating (Low to High)</button>
 <button class="btn btn-outline-black decrease"
-onclick="sortRatingDecreasing()"
+onclick="sort('rdesc')"
 type="button">Rating (High to Low)</button>
                                 </div>
                             </div>
@@ -113,7 +113,7 @@ type="button">Rating (High to Low)</button>
 
 
 
-const sortPriceIncreasing=async()=>{
+const sort=async(param)=>{
 
         const productss = await getAllProducts();
         var newprod = document.getElementById('shop-products');
@@ -126,8 +126,19 @@ const sortPriceIncreasing=async()=>{
         append.appendChild(newprod);
         sorting();
         var products = await productss.slice(0);
-        products.sort(function(a,b) {
-            return a.price - b.price;
+        products.sort(function(a,b,) {
+            if(this.param==='pasc'){
+                return a.price - b.price;
+            }
+            else if(this.param==='pdesc'){
+                return b.price - a.price;
+            }
+            else if(this.param==='rasc'){
+                return a.rating.rate - b.rating.rate;
+            }
+            else if(this.param==='rdesc'){
+                return b.rating.rate - a.rating.rate;
+            }
         });
         console.log(products);
         await products.forEach(product => {
@@ -158,143 +169,3 @@ const sortPriceIncreasing=async()=>{
         })
         
     }
-
-    const sortPriceDecreasing=async()=>{
-
-        const productss = await getAllProducts();
-        var newprod = document.getElementById('shop-products');
-        newprod.remove();
-        var newprod=document.createElement('div');
-        newprod.className="row pb-3";
-        newprod.id="shop-products";
-        var append=document.getElementById('append');
-        append.appendChild(newprod);
-        sorting();
-        var products = await productss.slice(0);
-        products.sort(function(a,b) {
-            return b.price - a.price;
-        });
-        console.log(products);
-        await products.forEach(product => {
-            var div = document.createElement('div');
-            div.className='fil col-lg-3 col-md-6 col-sm-12 pb-1';
-            div.id='prod';
-            div.innerHTML=`
-            <div class="card product-item border-0 mb-4">
-                                <a href="details.html?id=${product.id}">
-                                <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                                        <img height='200px' width='200px' style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src=${product.image} alt="">
-                                </div>
-                                <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                                    <h6 class="text-truncate mb-3 title">${product.title}</h6>
-                                    <div class="d-flex justify-content-center">
-                                        <h6>$${product.price}</h6><h6 class="text-muted ml-2"><del>$${product.price}</del></h6>
-                                    </div>
-                                    <h6>Rating ${product.rating.rate}/5</h6>
-                                </div>
-                                </a>
-                                <div class="card-footer d-flex justify-content-between bg-light border">
-                                    <button class="btn btn-sm text-dark p-0" type="button" onclick="addToCart(${product.id})"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</button>
-                                    <button class="btn btn-sm text-dark p-0" type="submit" onclick="addToCart(${product.id}); window.location.href='cart.html'"><i class="fas fa-eye text-primary mr-1"></i>Buy Now</button>
-                                </div>
-                            </div>
-            `
-            newprod.appendChild(div);
-        })
-        
-    }
-
-    const sortRatingIncreasing=async()=>{
-
-        const productss = await getAllProducts();
-        var newprod = document.getElementById('shop-products');
-        newprod.remove();
-        var newprod=document.createElement('div');
-        newprod.className="row pb-3";
-        newprod.id="shop-products";
-        var append=document.getElementById('append');
-        append.appendChild(newprod);
-        sorting();
-        var products = await productss.slice(0);
-        products.sort(function(a,b) {
-            return a.rating.rate - b.rating.rate;
-        });
-        console.log(products);
-        await products.forEach(product => {
-            var div = document.createElement('div');
-            div.className='fil col-lg-3 col-md-6 col-sm-12 pb-1';
-            div.id='prod';
-            div.innerHTML=`
-            <div class="card product-item border-0 mb-4">
-                                <a href="details.html?id=${product.id}">
-                                <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                                        <img height='200px' width='200px' style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src=${product.image} alt="">
-                                </div>
-                                <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                                    <h6 class="text-truncate mb-3 title">${product.title}</h6>
-                                    <div class="d-flex justify-content-center">
-                                        <h6>$${product.price}</h6><h6 class="text-muted ml-2"><del>$${product.price}</del></h6>
-                                        
-                                    </div>
-                                    <h6>Rating ${product.rating.rate}/5</h6>
-                                </div>
-                                </a>
-                                <div class="card-footer d-flex justify-content-between bg-light border">
-                                    <button class="btn btn-sm text-dark p-0" type="button" onclick="addToCart(${product.id})"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</button>
-                                    <button class="btn btn-sm text-dark p-0" type="submit" onclick="addToCart(${product.id}); window.location.href='cart.html'"><i class="fas fa-eye text-primary mr-1"></i>Buy Now</button>
-                                </div>
-                            </div>
-            `
-            newprod.appendChild(div);
-        })
-       
-    }
-
-    const sortRatingDecreasing=async()=>{
-
-        const productss = await getAllProducts();
-        var newprod = document.getElementById('shop-products');
-        newprod.remove();
-        var newprod=document.createElement('div');
-        newprod.className="row pb-3";
-        newprod.id="shop-products";
-        var append=document.getElementById('append');
-        append.appendChild(newprod);
-        sorting();
-        var products = await productss.slice(0);
-        products.sort(function(a,b) {
-            return b.rating.rate - a.rating.rate;
-        });
-        console.log(products);
-        await products.forEach(product => {
-            var div = document.createElement('div');
-            div.className='fil col-lg-3 col-md-6 col-sm-12 pb-1';
-            div.id='prod';
-            div.innerHTML=`
-            <div class="card product-item border-0 mb-4">
-                                <a href="details.html?id=${product.id}">
-                                <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                                        <img height='200px' width='200px' style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src=${product.image} alt="">
-                                </div>
-                                <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                                    <h6 class="text-truncate mb-3 title">${product.title}</h6>
-                                    <div class="d-flex justify-content-center">
-                                        <h6>$${product.price}</h6><h6 class="text-muted ml-2"><del>$${product.price}</del></h6>
-                                    </div>
-                                    <h6>Rating ${product.rating.rate}/5</h6>
-                                </div>
-                                </a>
-                                <div class="card-footer d-flex justify-content-between bg-light border">
-                                    <button class="btn btn-sm text-dark p-0" type="button" onclick="addToCart(${product.id})"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</button>
-                                    <button class="btn btn-sm text-dark p-0" type="submit" onclick="addToCart(${product.id}); window.location.href='cart.html'"><i class="fas fa-eye text-primary mr-1"></i>Buy Now</button>
-                                </div>
-                            </div>
-            `
-            newprod.appendChild(div);
-        })
-        
-    }
-
-
-
-    
